2b022bdc60255df1e2872d7ce345d52d441581d2
Dinesh <dineshgarig@gmail.com>
2024-03-04 05:18:23 +0530

    Ui and add some functionality

--- router/list.js
+++ router/list.js
@@ -9,20 +9,29 @@ passport.use(new localStategy(usermodel.authenticate()))
 router.get("/",async function(req,res){
     res.render("signin.ejs")
 })
-
+router.get("/distroy/:id",async(req,res)=>{
+    let id = req.params.id;
+    console.log(id)
+    const deletepost = await postmodel.findByIdAndDelete({_id:id})
+    let userdata = await usermodel.findOne({username:req.session.passport.user})
+    userdata.posts.pop()
+    console.log(userdata)
+    console.log(deletepost)
+    res.redirect("/profile")
+})
 router.get("/login",async(req,res)=>{
     res.render("login.ejs")
 })
 
 router.get("/profile",isloggedIn,async(req,res)=>{
-    res.send("This is profile page")
+    res.render("profile.ejs")
 })
 router.post("/register",async function(req,res,next){
     let {username,fullname,email} = req.body;
     let userdata = new usermodel({username,fullname,email})
     usermodel.register(userdata,req.body.password).then(()=>{
         passport.authenticate("local")(req,res,function(){
-            res.redirect("/profile")
+            res.render("profile")
         })
     })
     
@@ -43,9 +52,19 @@ router.get("/logout",function(req,res,next){
         }
     })
 })
+router.post("/show",isloggedIn,async (req,res)=>{
+    const {desc} = req.body;
+    const userdata = await usermodel.findOne({username:req.session.passport.user}).populate("posts")
+    const postdata = await postmodel.create({desc})
+    userdata.posts.push(postdata._id)
+    // console.log(userdata)
+    await userdata.save()
+    res.render("display.ejs",{userdata})
+})
 
 function isloggedIn(req,res,next){
     if(req.isAuthenticated()) return next()
     res.redirect("/login")
 }
+
 module.exports = router
